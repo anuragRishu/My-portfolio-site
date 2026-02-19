ad8f8034972f38933757d98bfb16c585018fbbe1
Anurag Chaurasiya <145920720+anuragRishu@users.noreply.github.com>
2026-02-19 14:20:12 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,29 +1,46 @@
-
-import { Project, ProjectCategory } from './types';
+import { Project } from './types';
 
 export const PROJECTS: Project[] = [
   {
-    id: '1',
-    title: 'Neon Pulse: Tokyo Drift',
-    client: 'Cyberpunk Productions',
-    category: 'Cinematic',
-    thumbnail: 'https://images.unsplash.com/photo-1542204111-970c9227ba59?auto=format&fit=crop&q=80&w=800',
-    videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-    description: 'A high-energy, neon-soaked journey through Tokyo night streets using dynamic rhythm-based editing.',
-    tools: ['Premiere Pro', 'After Effects', 'Davinci Resolve'],
-    year: '2023'
+    "id": "1771470953648",
+    "title": "New Project Title",
+    "client": "Client Name",
+    "category": "Cinematic",
+    "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?auto=format&fit=crop&q=80&w=800",
+    "videoUrl": "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "description": "Project description here...",
+    "tools": [
+      "Premiere Pro",
+      "After Effects"
+    ],
+    "year": "2026"
   },
   {
-    id: '2',
-    title: 'Echoes of the Wild',
-    client: 'Nature Now',
-    category: 'Documentary',
-    thumbnail: 'https://images.unsplash.com/photo-1547234935-80c7145ec969?auto=format&fit=crop&q=80&w=800',
-    videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-    description: 'A slow-burn documentary piece focusing on the intimate life of predators in the Savannah.',
-    tools: ['Davinci Resolve', 'Avid Media Composer'],
-    year: '2024'
+    "id": "1771470952034",
+    "title": "New Project Title",
+    "client": "Client Name",
+    "category": "Cinematic",
+    "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?auto=format&fit=crop&q=80&w=800",
+    "videoUrl": "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "description": "Project description here...",
+    "tools": [
+      "Premiere Pro",
+      "After Effects"
+    ],
+    "year": "2026"
+  },
+  {
+    "id": "2",
+    "title": "Echoes of the Wild",
+    "client": "Nature Now",
+    "category": "Documentary",
+    "thumbnail": "https://images.unsplash.com/photo-1547234935-80c7145ec969?auto=format&fit=crop&q=80&w=800",
+    "videoUrl": "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "description": "A slow-burn documentary piece focusing on the intimate life of predators in the Savannah.",
+    "tools": [
+      "Davinci Resolve",
+      "Avid Media Composer"
+    ],
+    "year": "2024"
   }
 ];
-
-export const CATEGORIES: ProjectCategory[] = ['Cinematic', 'Commercial', 'Music Video', 'Social Media', 'Documentary'];
